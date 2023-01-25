e424e41a88b89e4d0c08d9c95a69a7931114ec2c
juanjosegr <juanjosegr9@gmail.com>
2023-01-25 18:10:29 +0100

    Initial commit

---
+++ src/coches.kt
@@ -0,0 +1,24 @@
+class Coche (color:String, marca: String, modelo: String, caballos: Int, puertas: Int, matricula: String) {
+    val color: String
+    val marca: String
+    val modelo: String
+    val caballos: Int
+    val puertas: Int
+    val matricula: String
+    init {
+        this.color = color
+        this.marca = marca
+        this.modelo = modelo
+        this.caballos = caballos
+        this.puertas = puertas
+        this. matricula = matricula
+    }
+    fun imprimir(){
+        println("Tu coche es un $marca $modelo, de color $color, con $caballos cv, de $puertas puertas y con matricula de $matricula.")
+    }
+
+    override fun toString(): String {
+        return "Tu coche es un $marca $modelo, de color $color, con $caballos cv, de $puertas puertas y con matricula de $matricula."
+    }
+
+}
---
+++ src/main.kt
@@ -0,0 +1,6 @@
+fun main(){
+ val citroen = Coche("Azul","Citroen","C4",110,3,"8027DYZ")
+ citroen.imprimir()
+ val bmw = Coche("Gris","Bmw","330i",280,5,"2102DSB")
+ println(bmw.toString())
+}
